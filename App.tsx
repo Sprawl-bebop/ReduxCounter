8b8a8a7b1951ef8cac4074bdabdab7f6997b5d81
Your Name <you@example.com>
2023-09-05 16:36:14 +0545

    final todo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import {CounterApp} from "./src/CounterApp/screen/counterApp";
+import ProductMain from './src/ContextApiRedux/screens/ProductMain';
+import TodoApp from './src/TodoApp/screen/TodoScreen';
 
 
 function App() {
   return (
-    <CounterApp/>
+    // <CounterApp/>
+    // <ProductMain/>
+    <TodoApp/>
+
   );
 }
 
